b8a013b8e250fab5755a6631613c07e24106006a
Douglas Craig <dcraig112@gmail.com>
2018-05-18 19:30:09 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ end
 def input_to_index(user_input)
   index = user_input.to_i - 1
 end
-#
-# def move(board, index, selection="X")
-#   board[index] = selection
-# end
+
+def move(board, index, selection="X")
+  board[index] = selection
+end
